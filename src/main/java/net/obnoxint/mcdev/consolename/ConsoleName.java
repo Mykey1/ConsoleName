df7a3a32ca03f0ef1e47e3c973de7ad75534ee0b
obnoxint <mail@obnoxint.net>
2012-06-03 17:24:16 +0200

    Revert "Organized imports."
    This reverts commit 1d69990797d4f9058783b0443536e4626cc6c240.

--- src/main/java/net/obnoxint/mcdev/consolename/ConsoleName.java
+++ src/main/java/net/obnoxint/mcdev/consolename/ConsoleName.java
@@ -1,6 +1,7 @@
 package net.obnoxint.mcdev.consolename;
 
 import java.io.File;
+import java.io.IOException;
 
 import net.obnoxint.mcdev.feature.Feature;
 
@@ -111,18 +112,6 @@ public final class ConsoleName extends JavaPlugin implements Feature {
         setFeatureActive(true);
     }
 
-    void resetDefaultPrefix(CommandSender sender, String targetPlayer) {
-        String msg;
-        if (targetPlayer == null) {
-            getFeatureProperties().setPrefix(ConsoleNameProperties.PROPERTY_PREFIX_DEFAULT);
-            msg = "Broadcast prefix reset to: " + ConsoleNameProperties.PROPERTY_PREFIX_DEFAULT;
-        } else {
-            getFeatureProperties().setPrefix(targetPlayer, null);
-            msg = "Broadcast prefix of player " + targetPlayer + " removed.";
-        }
-        sender.sendMessage(msg);
-    }
-
     @Override
     public void setFeatureActive(boolean active) {
         if (this.active != active) {
@@ -133,6 +122,18 @@ public final class ConsoleName extends JavaPlugin implements Feature {
             }
             this.active = active;
         }
+    }
+
+    void resetDefaultPrefix(CommandSender sender, String targetPlayer) {
+        String msg;
+        if (targetPlayer == null) {
+            getFeatureProperties().setPrefix(ConsoleNameProperties.PROPERTY_PREFIX_DEFAULT);
+            msg = "Broadcast prefix reset to: " + ConsoleNameProperties.PROPERTY_PREFIX_DEFAULT;
+        } else {
+            getFeatureProperties().setPrefix(targetPlayer, null);
+            msg = "Broadcast prefix of player " + targetPlayer + " removed.";
+        }
+        sender.sendMessage(msg);
     }
 
 }
