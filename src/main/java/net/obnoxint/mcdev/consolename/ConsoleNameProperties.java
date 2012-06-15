dbef00734d87eefb7de184806b7ece8107be8f30
obnoxint <mail@obnoxint.net>
2012-06-16 00:42:22 +0200

    Added properties for sign-broadcast feature.

--- src/main/java/net/obnoxint/mcdev/consolename/ConsoleNameProperties.java
+++ src/main/java/net/obnoxint/mcdev/consolename/ConsoleNameProperties.java
@@ -5,23 +5,29 @@ import java.util.HashMap;
 import net.obnoxint.mcdev.feature.FeatureProperties;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
 public final class ConsoleNameProperties extends FeatureProperties {
 
     public static final String PROPERTY_CHATFORMATSYMBOL_DEFAULT = "&";
     public static final String PROPERTY_PREFIX_DEFAULT = ChatColor.ITALIC.toString() + ChatColor.GOLD.toString() + "[Console]" + ChatColor.RESET.toString() + ":";
+    public static final int PROPERTY_SIGNBROADCASTTOOLID_DEFAULT = Material.BOOK.getId(); // 340
 
     private static final String CHAT_FORMAT_SYMBOL = "§";
     private static final String PROPERTY_CHATFORMATSYMBOL_NAME = "chatFormatSymbol";
     private static final String PROPERTY_OVERRIDESAYCOMMAND_NAME = "overrideSayCommand";
     private static final String PROPERTY_PERPLAYER_NAME_PREFIX = "player_";
     private static final String PROPERTY_PREFIX_NAME = "prefix";
-    private String chatFormatSymbol = PROPERTY_CHATFORMATSYMBOL_DEFAULT;
+    private static final String PROPERTY_ENABLESIGNBROADCAST_NAME = "enableSignBroadcast";
+    private static final String PROPERTY_SIGNBROADCASTTOOLID_NAME = "signBroadcastToolId";
 
+    private String chatFormatSymbol = PROPERTY_CHATFORMATSYMBOL_DEFAULT;
     private boolean overrideSayCommand = false;
     private String prefix = PROPERTY_PREFIX_DEFAULT;
     private HashMap<String, String> prefixes = new HashMap<>();
+    private boolean enableSignBroadcast = false;
+    private int signBroadcastToolId = PROPERTY_SIGNBROADCASTTOOLID_DEFAULT;
 
     ConsoleNameProperties(ConsoleName feature) {
         super(feature);
@@ -50,6 +56,14 @@ public final class ConsoleNameProperties extends FeatureProperties {
         return (r == null) ? prefix : r;
     }
 
+    public Material getSignBroadcastTool() {
+        return Material.getMaterial(signBroadcastToolId);
+    }
+
+    public boolean isEnableSignBroadcast() {
+        return enableSignBroadcast;
+    }
+
     /**
      * @return true if the 'say' command is being overriden by the 'bc' command.
      */
@@ -67,6 +81,13 @@ public final class ConsoleNameProperties extends FeatureProperties {
                 this.chatFormatSymbol = chatFormatSymbol;
                 setDirty();
             }
+        }
+    }
+
+    public void setEnableSignBroadcast(boolean enableSignBroadcast) {
+        if (this.enableSignBroadcast != enableSignBroadcast) {
+            this.enableSignBroadcast = enableSignBroadcast;
+            setDirty();
         }
     }
 
@@ -106,6 +127,14 @@ public final class ConsoleNameProperties extends FeatureProperties {
         }
     }
 
+    public void setSignBroadcastTool(Material signBroadcastTool) {
+        int id = signBroadcastTool.getId();
+        if (this.signBroadcastToolId != id) {
+            this.signBroadcastToolId = id;
+            setDirty();
+        }
+    }
+
     @Override
     protected void onFileCreated() {
         getProperties().setProperty(PROPERTY_OVERRIDESAYCOMMAND_NAME, String.valueOf(overrideSayCommand));
@@ -118,6 +147,15 @@ public final class ConsoleNameProperties extends FeatureProperties {
         overrideSayCommand = Boolean.valueOf(getProperties().getProperty(PROPERTY_OVERRIDESAYCOMMAND_NAME, PROPERTY_DEFAULT_BOOLEAN_FALSE));
         chatFormatSymbol = getProperties().getProperty(PROPERTY_CHATFORMATSYMBOL_NAME, PROPERTY_CHATFORMATSYMBOL_DEFAULT);
         prefix = getProperties().getProperty(PROPERTY_PREFIX_NAME, PROPERTY_PREFIX_DEFAULT);
+        enableSignBroadcast = Boolean.valueOf(getProperties().getProperty(PROPERTY_ENABLESIGNBROADCAST_NAME, PROPERTY_DEFAULT_BOOLEAN_FALSE));
+
+        // load signBroadcastId
+        try {
+            int id = Integer.valueOf(getProperties().getProperty(PROPERTY_SIGNBROADCASTTOOLID_NAME));
+            if (Material.getMaterial(id) != null) {
+                signBroadcastToolId = id;
+            }
+        } catch (NumberFormatException e) {}
 
         // load per-player prefixes
         for (String prop : getProperties().stringPropertyNames()) {
@@ -135,6 +173,8 @@ public final class ConsoleNameProperties extends FeatureProperties {
         getProperties().setProperty(PROPERTY_OVERRIDESAYCOMMAND_NAME, String.valueOf(overrideSayCommand));
         getProperties().setProperty(PROPERTY_CHATFORMATSYMBOL_NAME, chatFormatSymbol);
         getProperties().setProperty(PROPERTY_PREFIX_NAME, prefix);
+        getProperties().setProperty(PROPERTY_ENABLESIGNBROADCAST_NAME, String.valueOf(enableSignBroadcast));
+        getProperties().setProperty(PROPERTY_SIGNBROADCASTTOOLID_NAME, String.valueOf(signBroadcastToolId));
 
         // store per-player prefixes
         for (String prop : prefixes.keySet()) {
